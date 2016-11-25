0fa1aa849fa297599618711408419c956b1d44be
yangchenglin <yangchenglin@huijiayou.cn>
2016-11-25 14:47:13 +0800

    2

--- example-ui/src/main/java/com/matrix/controller/ExampleController.java
+++ example-ui/src/main/java/com/matrix/controller/ExampleController.java
@@ -18,6 +18,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.github.miemiedev.mybatis.paginator.domain.Order;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.matrix.pojo.entity.UserDemo;
 import com.matrix.service.IExampleService;
 
 /**
@@ -39,134 +40,74 @@ private static Logger logger=Logger.getLogger(ExampleController.class);
 	/**
 	 * @descriptions 前往添加页面 addExample.jsp
 	 * 
-	 * @param session
-	 * @return
 	 * @author Yangcl
 	 * @date 2016年6月13日-下午10:53:55
 	 * @version 1.0.0.1
 	 */
 	@RequestMapping("addInfoPage")
 	public String toAddPage(HttpSession session){ 
-		// TODO 按钮权限控制等等
+		
 		return "jsp/example/addExample"; 
 	}
 	
 	/**
 	 * @descriptions 添加一条信息到数据库，成功后跳转回添加页面
 	 * 
-	 * @param entity
-	 * @return
 	 * @author Yangcl
 	 * @date 2016年6月13日-下午11:04:45
 	 * @version 1.0.0.1
 	 */
 	@RequestMapping("addInfo")
-	public String addInfo(Company entity , ModelMap model , HttpSession session){ 
-		if(StringUtils.isNotBlank(entity.getCompanyName()) && StringUtils.isNotBlank(entity.getRemark())){
-			entity.setFlag(1);
-			UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
-			entity.setCreateTime(new Date());
-			entity.setCreateUserId( userInfo.getId() ); 
-			entity.setUpdateTime(new Date());
-			entity.setUpdateUserId(userInfo.getId()); 
-			
-			Integer count = companyService.insertSelective(entity);
-			if(count == 1){
-				model.put("status", true);
-				model.put("msg", "数据插入成功！");
-			}else{
-				model.put("status", false);
-				model.put("msg", "数据插入异常！");
-			}
-		}else{
-			model.put("status", false);
-			model.put("msg", "数据不可为空！");
-		}
+	public String addInfo(UserDemo entity , ModelMap model , HttpSession session){ 
+//		entity.setFlag(1);
+//		UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
+//		entity.setCreateTime(new Date());
+//		entity.setCreateUserId( userInfo.getId() ); 
+//		entity.setUpdateTime(new Date());
+//		entity.setUpdateUserId(userInfo.getId()); 
+//		
+//		Integer count = companyService.insertSelective(entity);
+//		if(count == 1){
+//			model.put("status", true);
+//			model.put("msg", "数据插入成功！");
+//		}else{
+//			model.put("status", false);
+//			model.put("msg", "数据插入异常！");
+//		}
 		
 		return "jsp/example/addExample"; 
 	}
 	
 	
 	/**
-	 * @descriptions 简单分页示例 不涉及弹窗分页问题 对应 pageFormExample.jsp
-	 * 
-	 * @param info
-	 * @param model
-	 * @param request
-	 * @param session
-	 * @return
-	 * @refactor no
-	 * @date 2016年6月3日下午7:27:51
-	 * @author Yangcl 
-	 * @version 1.0.0.1
-	 */
-	@RequestMapping("pageFormExample")   
-	public String toPageFormExample(UserInfo info , ModelMap model , HttpServletRequest request, HttpSession session){ 
-		logger.info(" to pageFormExample.jsp  ... "); 
-		
-		String pageNum = request.getParameter("pageNum"); // 当前第几页
-		String pageSize = request.getParameter("pageSize");  // 当前页所显示记录条数
-		
-		int num = 1;
-		int size = 10;
-		if (StringUtils.isNotBlank(pageNum)) {
-			num = Integer.parseInt(pageNum);
-		}
-		if (StringUtils.isNotBlank(pageSize)) {
-			size = Integer.parseInt(pageSize);
-		}
-		
-		UserInfo entity = new UserInfo();
-//		entity.setSex(1); 
-		String sortString = "id.desc";
-		Order.formString(sortString);
-		PageHelper.startPage(num, size);
-		List<UserInfo> userList = userInfoService.queryPage(entity);
-		if(userList != null && userList.size() > 0){
-			PageInfo<UserInfo> pageList = new PageInfo<UserInfo>(userList);
-			model.put("pageList", pageList); 
-			model.put("status", true);  // 为了开发标准化 status标记必须添加 
-		}else{
-			model.put("status", false); 
-		}
-		
-		
-		return "jsp/example/pageFormExample"; 
-	}
-	
-	/**
 	 * @descriptions 简单分页示例 Ajax分页 不涉及弹窗分页问题 
 	 * 
-	 * @param session 
 	 * @date 2016年8月17日下午5:44:52
 	 * @author Yangcl 
 	 * @version 1.0.0.1
 	 */
 	@RequestMapping("ajaxFormExample")
 	public String toAjaxFormExample(HttpSession session){ 
-		// TODO 按钮权限控制等等
 		return "jsp/example/ajaxFormExample"; 
 	}
 	
 	@RequestMapping(value = "ajaxPageData", produces = { "application/json;charset=utf-8" })
 	@ResponseBody
-	public JSONObject ajaxPageData(UserInfo entity , HttpServletRequest request, HttpSession session){
+	public JSONObject ajaxPageData(UserDemo entity , HttpServletRequest request, HttpSession session){
 		logger.info(" to ajaxFormExample.jsp  ... "); 
-		return userInfoService.ajaxPageData(entity, request);  
+		return exampleService.ajaxPageData(entity, request);  
 	}
 	
 	
 	/**
 	 * @descriptions Ajax分页 且弹窗同时分页
 	 * 
-	 * @param session 
 	 * @date 2016年8月17日下午5:44:52
 	 * @author Yangcl 
 	 * @version 1.0.0.1
 	 */
 	@RequestMapping("ajaxFormDialogExample")
 	public String toAjaxFormDialogExample(HttpSession session){ 
-		// TODO 按钮权限控制等等
 		return "jsp/example/ajaxFormDialogExample"; 
 	}
 	
@@ -191,26 +132,28 @@ private static Logger logger=Logger.getLogger(ExampleController.class);
 	
 	@RequestMapping(value = "deleteOne", produces = { "application/json;charset=utf-8" })
 	@ResponseBody
-	public JSONObject deleteOne(UserInfo info){
-		return userInfoService.deleteOne(info); 
+	public JSONObject deleteOne(UserDemo info){
+//		return exampleService.deleteOne(info); 
+		return null;
 	}
 	
 	@RequestMapping("editInfoPage")   
-	public String editInfoPage(UserInfo info , ModelMap model , HttpServletRequest request, HttpSession session){
-		if(StringUtils.isNotBlank(info.getId().toString())){
-			UserInfo entity = userInfoService.find(info.getId());
-			if(entity != null){
-				model.addAttribute("entity", entity);
-			}
-		}
+	public String editInfoPage(UserDemo info , ModelMap model , HttpServletRequest request, HttpSession session){
+//		if(StringUtils.isNotBlank(info.getId().toString())){
+//			UserInfo entity = userInfoService.find(info.getId());
+//			if(entity != null){
+//				model.addAttribute("entity", entity);
+//			}
+//		}
 		
 		return "jsp/example/editExample"; 
 	}
 	
 	@RequestMapping(value = "editInfo", produces = { "application/json;charset=utf-8" })
 	@ResponseBody
-	public JSONObject editInfo(UserInfo info){
-		return userInfoService.editInfo(info);
+	public JSONObject editInfo(UserDemo info){
+//		return userInfoService.editInfo(info);
+		return null;
 	}
 
 }
