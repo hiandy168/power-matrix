b0717e5c6d2efd523a1c9235399b4e8240b91d1b
Yangcl <794867067@qq.com>
2017-03-06 23:03:47 +0800

    2

--- center-educational/src/main/java/com/matrix/service/impl/EducationalServiceImpl.java
+++ center-educational/src/main/java/com/matrix/service/impl/EducationalServiceImpl.java
@@ -1,10 +1,29 @@
 package com.matrix.service.impl;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
+import com.matrix.dao.ITLessonDao;
+import com.matrix.dao.ITLessonSignDao;
+import com.matrix.dao.ITStudentDao;
+import com.matrix.dao.ITTeacherDao;
 import com.matrix.service.IEducationalService;
 
 @Service
 public class EducationalServiceImpl implements IEducationalService {
 
+	@Resource
+	private ITLessonSignDao lessonSignDao;
+	
+	@Resource
+	private ITTeacherDao teacherDao;
+	
+	@Resource
+	private ITStudentDao sutdentDao;
+	
+	@Resource 
+	private ITLessonDao lessonDao;
+	
+	
 }
