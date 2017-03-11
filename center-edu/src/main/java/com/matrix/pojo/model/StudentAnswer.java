c29c6eeab14b38550cbbe6cdc5e8bbead16256bd
Yangcl <794867067@qq.com>
2017-03-11 18:31:56 +0800

    2

---
+++ center-edu/src/main/java/com/matrix/pojo/model/StudentAnswer.java
@@ -0,0 +1,19 @@
+package com.matrix.pojo.model;
+
+import java.util.List;
+
+/**
+ * @descriptions 学生答案模型
+ *
+ * @author Yangcl 
+ * @home https://github.com/PowerYangcl
+ * @date 2017年3月11日 下午5:36:05
+ * @version 1.0.1
+ */
+public class StudentAnswer {
+	private String paperCode;
+	private String studentCode;
+	private List<Answer> list; 
+	
+	
+}
