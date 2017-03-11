3a73baaf79bd3949385a83348df01385bb5d4b74
Yangcl <794867067@qq.com>
2017-03-11 12:48:36 +0800

    2

---
+++ center-edu/src/main/java/com/matrix/pojo/entity/TExamPaper.java
@@ -0,0 +1,85 @@
+package com.matrix.pojo.entity;
+
+import java.util.Date;
+
+public class TExamPaper {
+    private Integer id;
+    private String uuid;
+    private String code;
+    private String teacherCode;
+    private String lessonCode;
+    private String questionCode;
+    private Double score;
+    private String createUser;
+    private Date createTime;
+    private String updateUser;
+    private Date updateTime;
+    
+    
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getTeacherCode() {
+		return teacherCode;
+	}
+	public void setTeacherCode(String teacherCode) {
+		this.teacherCode = teacherCode;
+	}
+	public String getLessonCode() {
+		return lessonCode;
+	}
+	public void setLessonCode(String lessonCode) {
+		this.lessonCode = lessonCode;
+	}
+	public String getQuestionCode() {
+		return questionCode;
+	}
+	public void setQuestionCode(String questionCode) {
+		this.questionCode = questionCode;
+	}
+	public Double getScore() {
+		return score;
+	}
+	public void setScore(Double score) {
+		this.score = score;
+	}
+	public String getCreateUser() {
+		return createUser;
+	}
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public String getUpdateUser() {
+		return updateUser;
+	}
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+}
---
+++ center-edu/src/main/java/com/matrix/pojo/entity/TExamQuestions.java
@@ -0,0 +1,105 @@
+package com.matrix.pojo.entity;
+
+import java.util.Date;
+
+public class TExamQuestions {
+    private Integer id;
+    private String uuid;
+    private String code;
+    private String deptCode;
+    private String professionCode;
+    private String lessonCode;
+    private String url;
+    private Double score;
+    private String createUser;
+    private Date createTime;
+    private String updateUser;
+    private Date updateTime;
+    private String question;
+    private String answer;
+    
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getDeptCode() {
+		return deptCode;
+	}
+	public void setDeptCode(String deptCode) {
+		this.deptCode = deptCode;
+	}
+	public String getProfessionCode() {
+		return professionCode;
+	}
+	public void setProfessionCode(String professionCode) {
+		this.professionCode = professionCode;
+	}
+	public String getLessonCode() {
+		return lessonCode;
+	}
+	public void setLessonCode(String lessonCode) {
+		this.lessonCode = lessonCode;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public Double getScore() {
+		return score;
+	}
+	public void setScore(Double score) {
+		this.score = score;
+	}
+	public String getCreateUser() {
+		return createUser;
+	}
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public String getUpdateUser() {
+		return updateUser;
+	}
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+}
