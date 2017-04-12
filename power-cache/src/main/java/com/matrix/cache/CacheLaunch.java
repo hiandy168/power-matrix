d6f008eb759a9e0187fbbd834f49cccdbe5500e0
Yangcl <794867067@qq.com>
2017-04-12 23:11:39 +0800

    2

--- power-cache/src/main/java/com/matrix/cache/CacheLaunch.java
+++ power-cache/src/main/java/com/matrix/cache/CacheLaunch.java
@@ -6,30 +6,37 @@ import com.matrix.cache.servletContext.launch.ContextLaunch;
 
 /**
  * @description: 缓存对外服务入口|单例
+ * 	
+ * 	使用cglib动态为Java类添加方法
+ * 	http://rensanning.iteye.com/blog/1924274
+ * 
+ * 	https://zhidao.baidu.com/question/1540712568691888627.html
  * 
  * @author Yangcl
  * @home https://github.com/PowerYangcl
  * @date 2017年4月12日 下午6:51:17 
  * @version 1.0.0
  */
-public class CacheLaunch extends BaseClass{ 
-	private ContextLaunch contextLaunch;
-	private RedisLaunch redisLaunch;   
-
-	public static final CacheLaunch getInstance() {
-		return LazyHolder.INSTANCE; 
+public class CacheLaunch<T> extends BaseClass{ 
+ 
+	private T launch;
+	
+	private void setLaunch(T t){
+		this.launch = t;
 	}
-	private CacheLaunch() {
+
+	public T getLaunch(){
+		return launch;
+	} 
+	
+	public CacheLaunch() {
 		String cacheLaunchType = this.getConfig("power-cache.cache_launch_type");
 		if(cacheLaunchType.equals("redis")){
-			
+			this.setLaunch((T) new RedisLaunch());
 		}else if(cacheLaunchType.equals("application")){
-			
+			this.setLaunch((T) new ContextLaunch());  
 		}
-	}
-	private static class LazyHolder {
-		private static final CacheLaunch INSTANCE = new CacheLaunch();
-	}
+	} 
 	
 	
 	
