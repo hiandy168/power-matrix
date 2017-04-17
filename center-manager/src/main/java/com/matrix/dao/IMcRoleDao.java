3ff99b5571e659d1ee8edb9460662994256473b6
Yangcl <794867067@qq.com>
2017-04-17 23:55:51 +0800

    2

--- center-manager/src/main/java/com/matrix/dao/IMcRoleDao.java
+++ center-manager/src/main/java/com/matrix/dao/IMcRoleDao.java
@@ -1,8 +1,13 @@
 package com.matrix.dao;
 
+import java.util.List;
+
 import com.matrix.base.interfaces.IBaseDao;
+import com.matrix.pojo.dto.McRoleDto;
 import com.matrix.pojo.entity.McRole;
 
 public interface IMcRoleDao extends IBaseDao<McRole, Integer>{
 	public Integer insertSelectiveGetZid(McRole e);
+	
+	public List<McRoleDto> findMcRoleDtoList();
 }
--- power-cache/src/main/java/com/matrix/system/DictionaryCacheSupport.java
+++ power-cache/src/main/java/com/matrix/system/DictionaryCacheSupport.java
@@ -1,12 +1,10 @@
 package com.matrix.system;
 
-import java.io.File;
-
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import com.matrix.base.BaseClass;
-import com.matrix.system.cache.SysWorkDir;
-import com.matrix.util.IoUtil;
+import com.matrix.base.interfaces.IBaseDictCache;
+import com.matrix.system.init.RootInit;
 
 /**
  * @description: 为DictionaryTableCacheInit.java 提供远程火力 
@@ -18,10 +16,27 @@ import com.matrix.util.IoUtil;
  */
 public class DictionaryCacheSupport extends BaseClass{
 
+	
 	public boolean supportInit(){
 		try {
 			// TODO 加载缓存数据字典相关的表中的内容到Redis中
-			
+			String cacheLaunchType = this.getConfig("power-cache.cache_launch_type");
+			if(cacheLaunchType.equals("redis")){ 
+				
+			}else if(cacheLaunchType.equals("context")){ 
+				String package_ = this.getConfig("power-cache.default_package_url");
+				String dictCacheClass = this.getConfig("power-cache.sub_project_cache_init");
+				if(StringUtils.isNotBlank(dictCacheClass)){
+					String [] arr = dictCacheClass.split(",");
+					for(int i = 0 ; i < arr.length ; i ++){
+						Class<?> clazz = Class.forName(package_ + arr[i]);   
+						if (clazz != null && clazz.getDeclaredMethods() != null){
+							RootInit dictCache = (RootInit) clazz.newInstance();
+							dictCache.init();
+						}
+					}
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
