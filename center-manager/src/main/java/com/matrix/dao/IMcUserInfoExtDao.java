da98eec9b5a2eb92ace7df582d8872500d17e00e
Yangcl <794867067@qq.com>
2017-05-30 22:12:10 +0800

    加入用户扩展信息基础类mc_user_info_ext

---
+++ center-manager/src/main/java/com/matrix/dao/IMcUserInfoExtDao.java
@@ -0,0 +1,8 @@
+package com.matrix.dao;
+
+import com.matrix.base.interfaces.IBaseDao;
+import com.matrix.pojo.entity.McUserInfoExt;
+
+public interface IMcUserInfoExtDao extends IBaseDao<McUserInfoExt , Integer>{
+
+}
---
+++ center-manager/src/main/java/com/matrix/pojo/entity/McUserInfoExt.java
@@ -0,0 +1,238 @@
+package com.matrix.pojo.entity;
+
+/**
+ * @descriptions 用户扩展信息相关|param a ~ t 用于后期扩展使用，可以直接替换，方便在xml中操作
+ *
+ * @author Yangcl 
+ * @home https://github.com/PowerYangcl
+ * @date 2017年5月30日 下午10:08:42
+ * @version 1.0.1
+ */
+public class McUserInfoExt {
+    private Integer id;
+    private Integer userInfoId;
+    private String picUrl;
+    private String pageCss;
+    private String platform;
+
+    private String parama;
+    private String paramb;
+    private String paramc;
+    private String paramd;
+    private String parame;
+    private String paramf;
+    private String paramg;
+    private String paramh;
+    private String parami;
+    private String paramj;
+    private String paramk;
+    private String paraml;
+    private String paramm;
+    private String paramn;
+    private String paramo;
+    private String paramp;
+    private String paramq;
+    private String paramr;
+    private String params;
+    private String paramt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserInfoId() {
+        return userInfoId;
+    }
+
+    public void setUserInfoId(Integer userInfoId) {
+        this.userInfoId = userInfoId;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl == null ? null : picUrl.trim();
+    }
+
+    public String getPageCss() {
+        return pageCss;
+    }
+
+    public void setPageCss(String pageCss) {
+        this.pageCss = pageCss == null ? null : pageCss.trim();
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform == null ? null : platform.trim();
+    }
+
+    public String getParama() {
+        return parama;
+    }
+
+    public void setParama(String parama) {
+        this.parama = parama == null ? null : parama.trim();
+    }
+
+    public String getParamb() {
+        return paramb;
+    }
+
+    public void setParamb(String paramb) {
+        this.paramb = paramb == null ? null : paramb.trim();
+    }
+
+    public String getParamc() {
+        return paramc;
+    }
+
+    public void setParamc(String paramc) {
+        this.paramc = paramc == null ? null : paramc.trim();
+    }
+
+    public String getParamd() {
+        return paramd;
+    }
+
+    public void setParamd(String paramd) {
+        this.paramd = paramd == null ? null : paramd.trim();
+    }
+
+    public String getParame() {
+        return parame;
+    }
+
+    public void setParame(String parame) {
+        this.parame = parame == null ? null : parame.trim();
+    }
+
+    public String getParamf() {
+        return paramf;
+    }
+
+    public void setParamf(String paramf) {
+        this.paramf = paramf == null ? null : paramf.trim();
+    }
+
+    public String getParamg() {
+        return paramg;
+    }
+
+    public void setParamg(String paramg) {
+        this.paramg = paramg == null ? null : paramg.trim();
+    }
+
+    public String getParamh() {
+        return paramh;
+    }
+
+    public void setParamh(String paramh) {
+        this.paramh = paramh == null ? null : paramh.trim();
+    }
+
+    public String getParami() {
+        return parami;
+    }
+
+    public void setParami(String parami) {
+        this.parami = parami == null ? null : parami.trim();
+    }
+
+    public String getParamj() {
+        return paramj;
+    }
+
+    public void setParamj(String paramj) {
+        this.paramj = paramj == null ? null : paramj.trim();
+    }
+
+    public String getParamk() {
+        return paramk;
+    }
+
+    public void setParamk(String paramk) {
+        this.paramk = paramk == null ? null : paramk.trim();
+    }
+
+    public String getParaml() {
+        return paraml;
+    }
+
+    public void setParaml(String paraml) {
+        this.paraml = paraml == null ? null : paraml.trim();
+    }
+
+    public String getParamm() {
+        return paramm;
+    }
+
+    public void setParamm(String paramm) {
+        this.paramm = paramm == null ? null : paramm.trim();
+    }
+
+    public String getParamn() {
+        return paramn;
+    }
+
+    public void setParamn(String paramn) {
+        this.paramn = paramn == null ? null : paramn.trim();
+    }
+
+    public String getParamo() {
+        return paramo;
+    }
+
+    public void setParamo(String paramo) {
+        this.paramo = paramo == null ? null : paramo.trim();
+    }
+
+    public String getParamp() {
+        return paramp;
+    }
+
+    public void setParamp(String paramp) {
+        this.paramp = paramp == null ? null : paramp.trim();
+    }
+
+    public String getParamq() {
+        return paramq;
+    }
+
+    public void setParamq(String paramq) {
+        this.paramq = paramq == null ? null : paramq.trim();
+    }
+
+    public String getParamr() {
+        return paramr;
+    }
+
+    public void setParamr(String paramr) {
+        this.paramr = paramr == null ? null : paramr.trim();
+    }
+
+    public String getParams() {
+        return params;
+    }
+
+    public void setParams(String params) {
+        this.params = params == null ? null : params.trim();
+    }
+
+    public String getParamt() {
+        return paramt;
+    }
+
+    public void setParamt(String paramt) {
+        this.paramt = paramt == null ? null : paramt.trim();
+    }
+}
