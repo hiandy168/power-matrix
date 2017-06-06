1eb366e982a75dd33d6fc808f13ab0f137445b01
yangchenglin <yangchenglin@huijiayou.cn>
2017-06-06 16:32:09 +0800

    2

--- center-edu/src/main/java/com/matrix/dao/ITStudentEvaluateDao.java
+++
@@ -1,9 +0,0 @@
-package com.matrix.dao;
-
-import com.matrix.base.interfaces.IBaseDao;
-import com.matrix.pojo.entity.TStudentEvaluate;
-
-public interface ITStudentEvaluateDao extends IBaseDao<TStudentEvaluate, Integer> {
-
-	TStudentEvaluate selectByPrimaryIds(Integer id);
-}
