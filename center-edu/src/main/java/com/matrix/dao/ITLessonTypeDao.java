504d9b67de88899cc0a92fb2aa00a2a6da4edd0a
yangchenglin <yangchenglin@huijiayou.cn>
2017-06-06 16:11:48 +0800

    纯页面跳转类型的基础项目框架，不是iframe形式的

--- center-edu/src/main/java/com/matrix/dao/ITLessonTypeDao.java
+++
@@ -1,8 +0,0 @@
-package com.matrix.dao;
-
-import com.matrix.base.interfaces.IBaseDao;
-import com.matrix.pojo.entity.TLessonType;
-
-public interface ITLessonTypeDao extends IBaseDao<TLessonType,  Integer>{ 
-
-}
--- center-edu/src/main/java/com/matrix/pojo/view/LessonSignView.java
+++
@@ -1,7 +0,0 @@
-package com.matrix.pojo.view;
-
-import com.matrix.pojo.entity.TLessonSign;
-
-public class LessonSignView extends TLessonSign{
-
-}
