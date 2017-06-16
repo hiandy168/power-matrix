66d582e5dea1ecf5326ec44661ceba1d7384bc7c
yangchenglin <yangchenglin@huijiayou.cn>
2017-06-16 15:55:24 +0800

    媒体站 分类管理

--- center-media/src/main/java/com/matrix/service/impl/McArticleTypeServiceImpl.java
+++ center-media/src/main/java/com/matrix/service/impl/McArticleTypeServiceImpl.java
@@ -1,9 +1,20 @@
 package com.matrix.service.impl;
 
-import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.matrix.base.BaseServiceImpl;
 import com.matrix.dao.IMcArticleInfoDao;
 import com.matrix.dao.IMcArticleTypeDao;
@@ -16,7 +27,40 @@ public class McArticleTypeServiceImpl extends BaseServiceImpl<McArticleType, Int
 	private IMcArticleInfoDao mcArticleInfoDao;
 	
 	@Resource
-	private IMcArticleTypeDao mcArticleTypeDao;  
+	private IMcArticleTypeDao mcArticleTypeDao;
+
+	/**
+	 * @description: 返回页面格式数据，同时 格式化时间 
+	 * 
+	 * @param e
+	 * @param request
+	 * @param session
+	 * @author Yangcl 
+	 * @date 2017年6月16日 下午3:15:12 
+	 * @version 1.0.0.1
+	 */
+	@Override
+	public JSONObject ajaxArticleAssortList(McArticleType e , HttpServletRequest request, HttpSession session) {
+		JSONObject r = super.ajaxPageData(e, request);
+		if(r.getString("status").equals("success")){
+			JSONObject data = r.getJSONObject("data");
+			JSONArray list = new JSONArray();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			for(int i = 0 ; i < data.getJSONArray("list").size() ; i ++){
+				Date ctime = data.getJSONArray("list").getJSONObject(i).getDate("createTime");
+				String cdate = sdf.format(ctime); 
+				Date utime = data.getJSONArray("list").getJSONObject(i).getDate("updateTime");
+				String udate = sdf.format(utime); 
+				JSONObject o = data.getJSONArray("list").getJSONObject(i);
+				o.put("createTime", cdate);
+				o.put("updateTime", udate);
+				list.add(o);
+			}  
+			data.put("list", list); 
+			r.put("data", data); 
+		}
+		return r;
+	}  
 	
 	
 }
