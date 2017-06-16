ca26ad16c2f63f66c493d6f26171497d6c2eb195
yangchenglin <yangchenglin@huijiayou.cn>
2017-06-16 18:04:18 +0800

    查询语句

---
+++ center-media/src/main/java/com/matrix/pojo/view/McArticleTypeView.java
@@ -0,0 +1,14 @@
+package com.matrix.pojo.view;
+
+import com.matrix.pojo.entity.McArticleType;
+
+public class McArticleTypeView extends McArticleType{
+	private Integer articleSum;
+
+	public Integer getArticleSum() {
+		return articleSum;
+	}
+	public void setArticleSum(Integer articleSum) {
+		this.articleSum = articleSum;
+	}
+}
