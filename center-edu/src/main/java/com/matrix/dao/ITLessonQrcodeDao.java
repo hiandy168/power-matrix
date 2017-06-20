8bf7ba4c9460d7f597b7776e36dc235f4da51f17
yangchenglin <yangchenglin@huijiayou.cn>
2017-06-20 17:22:01 +0800

    2

--- center-edu/src/main/java/com/matrix/dao/ITLessonQrcodeDao.java
+++
@@ -1,8 +0,0 @@
-package com.matrix.dao;
-
-import com.matrix.base.interfaces.IBaseDao;
-import com.matrix.pojo.entity.TLessonQrcode;
-
-public interface ITLessonQrcodeDao extends IBaseDao<TLessonQrcode, Integer> { 
-
-}
--- center-edu/src/main/java/com/matrix/dao/ITUserLoginDao.java
+++
@@ -1,8 +0,0 @@
-package com.matrix.dao;
-
-import com.matrix.base.interfaces.IBaseDao;
-import com.matrix.pojo.entity.TUserLogin;
-
-public interface ITUserLoginDao extends IBaseDao<TUserLogin, Integer> {
-
-}
